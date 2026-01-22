65739119957e0e1e882d86bdfc12d655a35e5ec9
wesqey <soccerwes2003@gmail.com>
2026-01-22 03:47:12 -0600

    apiKey" fix

--- src/components/PinAnonBoard.jsx
+++ src/components/PinAnonBoard.jsx
@@ -13,7 +13,7 @@ import {
 // ---------- Firebase Config ----------
 // You'll replace this with your own config from Firebase Console
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: import.meta.env._FIREBASE_API_KEY,
   authDomain: "pin-anon.firebaseapp.com",
   databaseURL: "https://pin-anon-default-rtdb.firebaseio.com",
   projectId: "pin-anon",
