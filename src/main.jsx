84b391cd1216928296f453e484ace672e190d1c5
wesqey <soccerwes2003@gmail.com>
2026-03-15 12:11:39 -0500

    carlisle mockup test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import PinAnonBoard from './pin-anon.jsx'  // ← Comment out
+import CarlisleSignupMockup from './CarlisleSignup.jsx'  // ← Add this
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CarlisleSignupMockup />  {/* ← Change this */}
   </React.StrictMode>,
 )
