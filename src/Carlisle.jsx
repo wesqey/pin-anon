4e2b3ff8af62f1c23f5b23d1b6a94ebe5d272d4a
wesqey <soccerwes2003@gmail.com>
2026-05-05 14:45:02 -0500

    prof fix

--- src/Carlisle.jsx
+++ src/Carlisle.jsx
@@ -1570,7 +1570,7 @@ export default function Carlisle() {
 
 function ProfilePicture({ authorId, author, size = 32, dark, profileImage }) {
   const colors = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8', '#F7DC6F', '#BB8FCE', '#85C1E2', '#F8B739', '#52B788'];
-  const id = authorId || author;
+  const id = authorId || author || 'default';
   const hash = id.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
   const color = colors[hash % colors.length];
   
