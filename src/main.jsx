bdc398cdba8559695bcbcee3e153461788235f73
wesqey <soccerwes2003@gmail.com>
2026-05-05 20:08:06 -0500

    main.jsx fix

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Carlisle from './Carlisle'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Carlisle />
+  </React.StrictMode>
+)
