f00696ce53b54d7783109ec9aee6e491c1cbfcbe
wesqey <soccerwes2003@gmail.com>
2026-05-09 21:31:22 -0500

    fish fr

--- src/Carlisle.jsx
+++ src/Carlisle.jsx
@@ -87,7 +87,7 @@ function loadUser() {
 }
 
 const EMPTY = {
-  rooms: [{ id: DEFAULT_ROOM, name: "main room", invite: DEFAULT_ROOM }, { id: "fish", name: "birds", invite: "birds", permanent: true }],
+  rooms: [{ id: DEFAULT_ROOM, name: "main room", invite: DEFAULT_ROOM }, { id: "fish", name: "fish", invite: "fish", permanent: true }],
   posts: [],
   settings: { whisper: false },
   inviteCodes: {},
